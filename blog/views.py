3d8abeae9b5b76686b2bfa7acfd63682e1734142
KlaudiaBC <klablaco@gmail.com>
2022-08-09 14:32:10 +0000

    Add views and render data

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def get_blog_one(request):
-    return render(request, 'blog/blog_one.html')
+    post_list = Post.objects.all()
+    context = {
+        "posts": post_list
+    }
+    return render(request, 'blog/blog_one.html', context)
