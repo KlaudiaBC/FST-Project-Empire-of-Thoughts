d5115bea9acecbc56e05bdd258e528e0d682fb0c
KlaudiaBC <klablaco@gmail.com>
2022-08-11 04:07:06 +0000

    Add <Edit Post> page

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
-from .views import PostList, PostView, AddPost
+from .views import PostList, PostView, AddPost, UpdatePost
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('view/<int:pk>', views.PostView.as_view(), name='post_view'),
-    path('add_post/', views.AddPost.as_view(), name="add_post")
+    path('add_post/', views.AddPost.as_view(), name="add_post"),
+    path('edit/<int:pk>', views.UpdatePost.as_view(), name="update"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 
@@ -19,3 +19,9 @@ class AddPost(CreateView):
     model = Post
     form_class = PostForm
     template_name = "add_post.html"
+
+
+class UpdatePost(UpdateView):
+    model = Post
+    template_name = "update.html"
+    fields = ['title', 'body']
