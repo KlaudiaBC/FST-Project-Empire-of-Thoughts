03162e83fde8ea400fec67ca9d4802ece2579fda
KlaudiaBC <klablaco@gmail.com>
2022-08-15 17:43:29 +0000

    Add more styling

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'body')
+        fields = ('title', 'body', 'author')
 
         widgets = {
             'title': forms.TextInput(attrs={
