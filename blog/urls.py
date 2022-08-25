6a1f581005e79102ba67134920ffa05c2f3c46ec
KlaudiaBC <klablaco@gmail.com>
2022-08-25 21:22:03 +0000

    Change url to css and add urls paths - heroku does not find them

--- blog/urls.py
+++ blog/urls.py
@@ -1,22 +1,26 @@
 """
 Define the URLs for the 'blog' application
 """
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('post/<int:pk>', views.PostView.as_view(), name='post_view'),
-    path('add_post/', views.AddPost.as_view(), name="add_post"),
-    path('edit/<int:pk>', views.UpdatePost.as_view(), name="update_post"),
-    path('delete/<int:pk>', views.DeletePost.as_view(), name="delete_post"),
-    path('like/<int:pk>', views.like_click, name="like_post"),
-    path('view/<int:pk>/comment',
+    path('blog/post/<int:pk>', views.PostView.as_view(), name='post_view'),
+    path('blog/add_post/', views.AddPost.as_view(), name="add_post"),
+    path('blog/edit/<int:pk>', views.UpdatePost.as_view(), name="update_post"),
+    path('blog/delete/<int:pk>', views.DeletePost.as_view(),
+         name="delete_post"),
+    path('blog/like/<int:pk>', views.like_click, name="like_post"),
+    path('blog/view/<int:pk>/comment',
          views.AddComment.as_view(), name="add_comment"),
-    path('category/<str:cats>', views.category_list, name="category"),
-    path('category-list/', views.category_list_view, name="category_list"),
-    path('post/<int:pk>/comment', views.AddComment.as_view(),
+    path('blog/category/<str:cats>', views.category_list, name="category"),
+    path('blog/category-list/', views.category_list_view,
+         name="category_list"),
+    path('blog/post/<int:pk>/comment', views.AddComment.as_view(),
          name="add_comment"),
-    path('mypage/', views.MyPosts.as_view(), name='my_page'),
+    path('blog/mypage/', views.MyPosts.as_view(), name='my_page'),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls'), name="members_urls"),
     ]
