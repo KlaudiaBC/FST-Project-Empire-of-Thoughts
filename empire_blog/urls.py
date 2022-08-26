d15c2ec635f35962aab1f59a6c71983c32eb7c3d
KlaudiaBC <klablaco@gmail.com>
2022-08-26 12:00:30 +0000

    Remove unused files from requirements, add urls to connect with static/

--- empire_blog/urls.py
+++ empire_blog/urls.py
@@ -4,6 +4,8 @@ Define the URLs for the project empire-blog
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,4 @@ urlpatterns = [
     path('', include('blog.urls'), name="blog_urls"),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'), name="members_urls"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
