6d299f615e2e94f8aa17a6363daa9578e2672aa7
KlaudiaBC <klablaco@gmail.com>
2022-08-28 20:04:15 +0000

    Continuation of fixing the Registration Form

--- members/forms.py
+++ members/forms.py
@@ -12,10 +12,10 @@ class SignUpForm(UserCreationForm):
     """
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs('class', 'form-control')
-        self.fields['email'].widget.attrs('class', 'form-control')
-        self.fields['password1'].widget.attrs('class', 'form-control')
-        self.fields['password2'].widget.attrs('class', 'form-control')
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
     class Meta:
         """
