34e1f66d25d8eb9e35857c44a280cad36611593f
KlaudiaBC <klablaco@gmail.com>
2022-08-28 18:30:44 +0000

    Fix bug in url

--- members/urls.py
+++ members/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from .views import UserRegisterView, terms_view
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view, name='register'),
+    path('register/', UserRegisterView.as_view(), name='register'),
     path('members/', include('django.contrib.auth.urls')),
     path('terms/', terms_view, name='terms')
 ]
