cac9a848713e1df9d477193818b15cddeef37ecb
KlaudiaBC <klablaco@gmail.com>
2022-08-28 18:28:40 +0000

    Update the url for changed registration view

--- members/urls.py
+++ members/urls.py
@@ -2,10 +2,10 @@
 Define the URLs for the 'blog' application
 """
 from django.urls import path, include
-from .views import signup, terms_view
+from .views import UserRegisterView, terms_view
 
 urlpatterns = [
-    path('register/', signup, name='register'),
+    path('register/', UserRegisterView.as_view, name='register'),
     path('members/', include('django.contrib.auth.urls')),
     path('terms/', terms_view, name='terms')
 ]
