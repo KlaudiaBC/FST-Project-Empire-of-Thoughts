e11c2b40dde327f42f3debddea0590d6df35e77c
KlaudiaBC <klablaco@gmail.com>
2022-08-28 21:29:07 +0000

    Deployment commit

--- empire_blog/settings.py
+++ empire_blog/settings.py
@@ -27,8 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'localhost']
 
