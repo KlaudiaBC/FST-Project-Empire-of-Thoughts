f2608c9ab27d5c3ca4f7dcc897f3e414f3e0ee43
KlaudiaBC <klablaco@gmail.com>
2022-08-29 08:40:40 +0000

    Try to set the static files dirs

--- empire_blog/settings.py
+++ empire_blog/settings.py
@@ -182,7 +182,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    "/workspace/FST-Project-Empire-of-Thoughts/static",
+    "/workspace/FST-Project-Empire-of-Thoughts/staticfiles",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
