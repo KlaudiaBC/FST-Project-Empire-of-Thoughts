d3b89362f4f904fb08dc4bb68898b32c7b5f1c2a
KlaudiaBC <klablaco@gmail.com>
2022-08-31 11:54:56 +0000

    Another approach to connect static to the django app

--- empire_blog/urls.py
+++ empire_blog/urls.py
@@ -5,21 +5,20 @@ Define the URLs for the project empire-blog
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('tinymce/', include('tinymce.urls')),
     path('', include('blog.urls'), name="blog_urls"),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'), name="members_urls"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    path(r'^media/(?P<path>.*)$', serve,
+         {'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,
+         {'document_root': settings.STATIC_ROOT}),
+]
 
 handler404 = "blog.views.handler404"
 handler500 = "blog.views.handler500"
-
-urlpatterns += staticfiles_urlpatterns()
